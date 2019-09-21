8f136a03b05f23808ffd4783f7aea8db189408cb
Adam Resnick <a.rezzy@gmail.com>
2019-09-21 16:33:26 -0400

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BalanceComponent } from './balance/balance.component';
+import { FaqComponent } from './faq/faq.component';
+import { RouterModule } from '@angular/router';
+import {appRoutes} from './app.routes';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BalanceComponent,
+    FaqComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(
+      appRoutes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/td-service.service.ts
@@ -0,0 +1,9 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TdServiceService {
+  apiKey: string = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJDQlAiLCJ0ZWFtX2lkIjoiNGU5YjhjYjEtYjNhZC0zYzNmLWJjNjItMTVkZTRmOGJjZDk0IiwiZXhwIjo5MjIzMzcyMDM2ODU0Nzc1LCJhcHBfaWQiOiJmNDYzNjA2Mi03YzkzLTQyMmItOTNhYi1kYmE5MmY2NzgzMjIifQ.2Ec3xxzowUx47TRsYVK4G2Mz4WDDpV46ZszKpknAokM"
+  constructor() { }
+}
